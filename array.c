c5f566621bf5459b79f5df1f81c4138974fc64ac
mechraj <94051997+mechraj@users.noreply.github.com>
2022-08-20 23:38:34 +0400

    List gets input from the user

--- array.c
+++ array.c
@@ -4,11 +4,11 @@
 
 int main(void)
 {
-    //Creating a list to store scores
+    //Creating a list to store scores and getting inputs from the user
     int scores[3];
-    scores[0]=50;
-    scores[1]=75;
-    scores[2]=50;
+    scores[0]=get_int("Score 1: ");
+    scores[1]=get_int("Score 2: ");
+    scores[2]=get_int("Score 3: ");
     
     printf("The sum is,%f", (scores[0]+scores[1]+scores[2])/3.0);
 }
