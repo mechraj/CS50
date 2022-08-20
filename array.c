a0842c398780c5b862e9c99dbf942bfab46eb8c1
mechraj <94051997+mechraj@users.noreply.github.com>
2022-08-21 00:10:03 +0400

    Used for loop to avoid repitions

--- array.c
+++ array.c
@@ -5,10 +5,12 @@
 int main(void)
 {
     //Creating a list to store scores and getting inputs from the user
-    int scores[3];
-    scores[0]=get_int("Score 1: ");
-    scores[1]=get_int("Score 2: ");
-    scores[2]=get_int("Score 3: ");
     
-    printf("The sum is,%f", (scores[0]+scores[1]+scores[2])/3.0);
+    int scores[3];
+    
+    for( int i=0;i<3;i++)
+    {
+        scores[i]=get_int("Scores: ");
+    }
+    printf("The average is, %f",(scores[0]+scores[1]+scores[2])/3.0);
 }
