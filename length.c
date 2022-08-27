0b0079eafa4515bdbeefaff3f3c39fd2bcb547bc
mechraj <94051997+mechraj@users.noreply.github.com>
2022-08-27 19:56:54 +0400

    add program to calculate length of string

---
+++ length.c
@@ -0,0 +1,20 @@
+//Program to determine the length of a string
+#include <stdio.h>
+#include <cs50.h>
+#include <cs50.c>
+
+int main(void)
+{
+    //Ask user for name
+
+    string name=get_string("Name: ");
+    //Intializing a variable
+    int i=0;
+    //checks for a null location
+    while(name[i]!=0)
+    {
+        i++;
+    }
+    printf("The number of letters in neme is, %i",i);
+}
+
