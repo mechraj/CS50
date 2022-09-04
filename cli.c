eaa5e6e9fde10db7ff0f6362c585c37ca98e173a
mechraj <94051997+mechraj@users.noreply.github.com>
2022-09-04 19:49:40 +0400

    Program with commend line arguments

---
+++ cli.c
@@ -0,0 +1,9 @@
+//program to take command line arguements//
+#include <cs50.h>
+#include <cs50.c>
+#include <stdio.h>
+
+int main(int argc, string argv[])
+{
+    printf("My name is %s,\n", argv[1]);
+}
