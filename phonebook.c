661f31078ef11015ce8a9c0e10588e1a0bd1a0c6
mechraj <94051997+mechraj@users.noreply.github.com>
2022-09-13 22:44:44 +0400

    Adds a new data structure

--- phonebook.c
+++ phonebook.c
@@ -4,20 +4,35 @@
 #include <cs50.c>
 #include <string.h>
 
+//defining a new data structure//
+typedef struct
+{
+    string name;
+    string number;
+}
+person;
+
 int main(void)
 {
-    //initializing empty list with names//
-    string names[]={"David","Carter"};
-    //Corresponding lists with numbers//
-    string numbers[]={"+1-617-495-1000", "+1-949-468-2750"};
+    person people[2];
+    {
+        //first set of entry into the new data structure//
+        people[0].name="David";
+        people[0].number="+1-617-495-1000";
+
+        //second entry
+        people[1].name="David";
+        people[1].number= "+1-949-468-2750";
+
+    }
 
     //looping through the list 
     for(int i=0;i<2;i++)
     {
         //comparing the names//
-        if(strcmp(names[i],"David")==0)
+        if(strcmp(people[i].name,"David")==0)
         {
-            printf("Found %s\n", numbers[i]);
+            printf("Found %s\n", people[i].number);
             return 0;
         }
     }
