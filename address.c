526943054100fae38ca5006e007f34e91a498dcd
mechraj <94051997+mechraj@users.noreply.github.com>
2022-10-09 17:59:28 +0400

    Adds a line to print the contents a partiular location in memory
    Using * as a derefernce operator

--- address.c
+++ address.c
@@ -1,8 +1,10 @@
-#include <stdio.h>
+#include<stdio.h>
 
 int main(void)
 {
-    int n=50;
-    int *p=&n;
-    printf("%p\n",p);
+    int n=50; //initializing a variable
+    int *p=&n; //using  a pointer to find the location of the variable n
+
+    printf("%p\n",p); //location of n in the memory
+    printf("%i\n",*p); // what is stored in that particular location 
 }
