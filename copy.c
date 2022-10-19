2e13b437b056dfad2bee5618c351063b6591a062
mechraj <94051997+mechraj@users.noreply.github.com>
2022-10-19 23:17:57 +0400

    Fixes the bug of string copy by using malloc

--- copy.c
+++ copy.c
@@ -3,14 +3,19 @@
 #include<stdio.h>
 #include<cs50.h>
 #include<cs50.c>
+#include<stdlib.h>
 
 int main(void)
 {
-    string s=get_string("Enter a word");
-    string t= s;
-    t[0]=toupper(t[0]);
+    char *s=get_string("s:");
+    char *t=malloc(strlen(s)+1);
 
-    printf("s=%s\n",s);
-    printf("t= %s\n",t);
+    for (int i=0, n=strlen(s)+1;i<n;i++)
+    {
+        t[i]=s[i];
+    }
+    t[0] = toupper(t[0]);
 
+    printf("s: %s\n", s);
+    printf("t: %s\n", t);
 }
